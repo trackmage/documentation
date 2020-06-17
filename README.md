b1dc4c110ea9fd869686770042df3344819baea3
karser <dpoddubny@gmail.com>
2020-06-17 19:35:41 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+<a href="#">
+  <div align="center">
+    <img src="https://user-images.githubusercontent.com/1675033/84406764-a7091300-ac12-11ea-8250-774a8f0697fe.jpg" width='128'/>
+  </div>
+</a>
+<h1 align="center">Trackmage - A tracking page for your store. Beautiful, branded, customizable.</h1>
+
+# TrackMage Documentation
+
+This website is built using [Docusaurus 2](https://v2.docusaurus.io/), a modern static website generator.
+
+### Installation
+
+```
+$ npm install
+```
+
+### Local Development
+
+```
+$ npm start
+```
+
+This command starts a local development server and open up a browser window. Most changes are reflected live without having to restart the server.
+
+### Build
+
+```
+$ npm build
+```
+
+This command generates static content into the `build` directory and can be served using any static contents hosting service.
+
+### Deployment
+
+```
+$ GIT_USER=<Your GitHub username> USE_SSH=true npm deploy
+```
+
+If you are using GitHub pages for hosting, this command is a convenient way to build the website and push to the `gh-pages` branch.
---
+++ docusaurus.config.js
@@ -0,0 +1,99 @@
+module.exports = {
+  title: 'TrackMage Documentation',
+  tagline: 'A tracking page for your store. Beautiful, branded, customizable',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  favicon: 'img/favicon.ico',
+  organizationName: 'TrackMage', // Usually your GitHub org/user name.
+  projectName: 'TrackMage', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'TrackMage Documentation',
+      logo: {
+        alt: 'TrackMage Logo',
+        src: 'img/logo.jpg',
+      },
+      links: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        {
+          href: 'https://api.trackmage.com/?ui=re_doc',
+          label: 'Api Reference',
+          position: 'left',
+        },
+        {
+          href: 'https://api.trackmage.com/',
+          label: 'Try API methods online',
+          position: 'left',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2/',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          // It is recommended to set document id as docs home page (`docs/` path).
+          homePageId: 'doc1',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
